8be422fb9a137f8da3b544e3e3aa4cbe46f6b76a
Dylan-Rick <dylan.rick@student.swic.edu>
2026-02-25 14:24:12 -0600

    Updated ternary

--- Ternary.js
+++ Ternary.js
@@ -1,9 +1,9 @@
 1. let access = hasKey ? "allowed" : "denied";
 
-2. score = 75
-   result = "pass" if score >= 60 else "fail";
-   print(result)
+2. let score = 75;
+   let result = (score >= 60) ? "pass" : "fail";
+   console.log(result);
 
 3. let username = "";
-   let displayName = (username.trim() === "") "Guest" : username;
+   let displayName = (username.trim() === "") ? "Guest" : username;
    console.log(`Welcome, ${displayName}!`);
