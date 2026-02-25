512676af48b2501d3d0896a61e65dbaece0b01ce
Dylan-Rick <dylan.rick@student.swic.edu>
2026-02-25 11:02:12 -0600

    Add ternary operator exercises

---
+++ Ternary.js
@@ -0,0 +1,9 @@
+1. let access = hasKey ? "allowed" : "denied";
+
+2. score = 75
+   result = "pass" if score >= 60 else "fail";
+   print(result)
+
+3. let username = "";
+   let displayName = (username.trim() === "") "Guest" : username;
+   console.log(`Welcome, ${displayName}!`);
